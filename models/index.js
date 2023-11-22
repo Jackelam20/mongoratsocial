efeaf64992c80ac4405649ec27468c327f25f152
Jackelam20 <jackelam20@gmail.com>
2023-11-22 11:57:17 -0600

    flet that

---
+++ models/index.js
@@ -0,0 +1,7 @@
+const Thought = require("./Thought");
+const User = require("./User");
+
+module.exports = {
+  User,
+  Thought,
+};
---
+++ routes/api/thoughtsRoutes.js
@@ -0,0 +1,21 @@
+const router = require("express").Router();
+const {
+  getAllThoughts,
+  getThoughtById,
+  createThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  removeReaction,
+} = require("../../controllers/thoughtControllers");
+
+router.route("/").get(getAllThoughts).post(createThought);
+router
+  .route("/:thoughtId")
+  .get(getThoughtById)
+  .put(updateThought)
+  .delete(deleteThought);
+
+router.route("/:thoughtId/reactions").post(addReaction).delete(removeReaction);
+
+module.exports = router;
